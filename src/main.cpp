75cd83054780c9a1a8ef606a7fd7496f167fadbb
Vincent Timofti <vincent.timofti@gmail.com>
2014-04-24 18:19:49 +0200

    initial

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  audioplayer
+//
+//  Created by Vincent Timofti on 24/04/2014.
+//  Copyright (c) 2014 Vincent Timofti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

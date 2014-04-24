b20284f9bdb2d413f52774d163a2d9562bb6bcf4
Vincent Timofti <vincent.timofti@gmail.com>
2014-04-24 20:14:01 +0200

    rm

--- audioplayer.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  audioplayer.cpp
-//  audioplayer
-//
-//  Created by Vincent Timofti on 24/04/2014.
-//  Copyright (c) 2014 Vincent Timofti. All rights reserved.
-//
-
-#include "audioplayer.h"
